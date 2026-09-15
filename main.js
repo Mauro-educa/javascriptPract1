ffa4d7296db6c2c6d094d952df5c574e5cff6531
HP <mauro.gargon@educa.jcyl.es>
2026-09-15 19:27:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Hola mundo");
+let x = 1; let y = 0;
+console.log("x = " + x + ", y= " + y);
+console.log("x + y = " + (x + y));
+console.log("x - y = " + (x - y));
+console.log("x * y = " + (x * y));
+console.log("x / y = " + (x / y));
