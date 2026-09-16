b3cc9df1a1c96fca96e41497f9b274332b7f86ee
Mauro <mauro.gargon@educa.cyl.es>
2026-09-16 08:50:38 +0200

    commit 2

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 console.log("Hola mundo");
-let x = 1; let y = 0;
-console.log("x = " + x + ", y= " + y);
-console.log("x + y = " + (x + y));
-console.log("x - y = " + (x - y));
-console.log("x * y = " + (x * y));
-console.log("x / y = " + (x / y));
+//
 //kks,s,s,
-//dición en clase
+//edición en clase sss
